f6d6e365eff97d7cec5db40707784899b50e3a27
Bob Hunt <bob.hunt.email@gmail.com>
2018-03-18 11:49:20 -0400

    spliced first random x & y into the first position of each array, replacing the value of 0.

--- mineSweeper.js
+++ mineSweeper.js
@@ -7,7 +7,7 @@ var grid = {
     height: 5,
     width: 5,
 
-    numberOfMines: 3,
+    numberOfMines: 5,
     minePositionsX: [0],
     minePositionsY: [0],
     minePosition: [0],
@@ -30,22 +30,39 @@ var grid = {
     // Randomly assign mines to the mineField. Do not allow duplicates.
     generateMines: function () {
 
-        for (k = 0; k <= this.numberOfMines; k++) {
+        for (k = 0; k <= this.numberOfMines-1; k++) {
             
             let y = Math.floor((Math.random() * this.height) + 1);
             let x = Math.floor((Math.random() * this.width) + 1);
-            console.log(x + " : " + y);
+            // console.log(x + " : " + y);
 
             let randomPosition = ("" + x + "-" + y);
-            console.log(randomPosition);
-            
+            // console.log(randomPosition);
+
                 if (this.minePosition.includes('randomPosition')) {
                     return;
                 } else {
-                    console.log("*" + randomPosition);
-                    this.minePositionsY.push(y);
-                    this.minePositionsX.push(x);
-                    this.minePosition.push(randomPosition);
+                    // if (this.minePosition[0] == 0){
+                    //     this.minePosition.splice(0,1,randomPosition)
+                    // }
+                    switch (this.minePosition[0]) {
+                        
+                        case 0:
+                            this.minePosition.splice(0,1,randomPosition)
+                            this.minePositionsY.splice(0,1,y)
+                            this.minePositionsX.splice(0,1,x)
+                            break;
+
+                        default: 
+                            this.minePositionsY.push(y)
+                            this.minePositionsX.push(x)
+                            this.minePosition.push(randomPosition)
+
+                    }
+                    // console.log("*" + randomPosition);
+                    // this.minePositionsY.push(y);
+                    // this.minePositionsX.push(x);
+                    // this.minePosition.push(randomPosition);
                 }
 
             // }
